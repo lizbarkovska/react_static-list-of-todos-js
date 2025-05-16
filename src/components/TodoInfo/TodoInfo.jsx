75793bed681b209e485bfb2ebc97a9b666938d17
Che <rererdom@gmail.com>
2025-05-16 17:22:34 +0100

    solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,32 @@
 // Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+import cn from 'classnames';
+import { UserInfo } from '../UserInfo';
+
+import todosFromServer from '../../api/todos.json';
+import usersFromServer from '../../api/users.json';
+
+function getUserById(userId) {
+  return usersFromServer.find(user => user.id === userId) || null;
+}
+
+export const todosNamed = todosFromServer.map(todo => ({
+  ...todo,
+  user: getUserById(todo.userId),
+}));
+
+export const TodoInfo = ({ todo }) => (
+  <article
+    className={cn('TodoInfo', {
+      'TodoInfo--completed': todo.completed === true,
+    })}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+
+    {todo.userId !== null && (
+      <UserInfo
+        user={getUserById(todo.userId)}
+        key={getUserById(todo.userId).id}
+      />
+    )}
+  </article>
+);
